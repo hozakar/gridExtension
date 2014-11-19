6549ebfe9eb805a9260e5693646f298a5aaccfc9
hozakar <hozakar@gmail.com>
2014-11-19 17:56:27 +0200

    Hello World!

--- content.js
+++ content.js
@@ -14,15 +14,56 @@
 
 	    chrome.storage.sync.get(null, function(answer) {
 	    	$.extend( true, defaults, answer );
-	    	for(var x in defaults) {
-	    		alert(defaults[x]);
-	    	}
+	    	
+	    	init();
 	    });
 	});
 
 	$(window).load(function() {
 	    chrome.storage.sync.get(null, function(answer) {
 	    	$.extend( true, defaults, answer );
+
+	    	init();
 	    });
 	});
+
+	function init() {
+		var btn = $('#grid_builder_toggle_button');
+		if(!btn.length) {
+			$('body').append('<div id="grid_builder_toggle_button"></div>');
+			$('#grid_builder_toggle_button').click(function(){
+				if($(this).data('open')) {
+					deleteGrid();
+					$(this).data('open', 0);
+				} else {
+					buildGrid();
+					$(this).data('open', 1);
+				}
+			});
+		}
+	}
+
+	function deleteGrid() {
+		$('.grid_builder').remove();
+	}
+
+	function buildGrid() {
+		var hor = parseInt(($('body').height() - defaults.topMargin) / defaults.lineHeight);
+		for(var i = 0; i < hor; i++) {
+			$('body').append(
+				'<div class="grid_builder grid_horizontal" style="top: ' + (i * defaults.lineHeight + defaults.topMargin)  + 'px;"></div>');
+		}
+
+		var colWidth = parseInt((defaults.pageWidth - (defaults.gutter * (defaults.columns - 1))) / defaults.columns);
+		var left = parseInt(($('body').width() - defaults.pageWidth) / 2);
+		$('body').append('<div class="grid_builder grid_vertical" style="left: ' + left  + 'px;"></div>');
+		left += colWidth; 
+		$('body').append('<div class="grid_builder grid_vertical" style="left: ' + left  + 'px;"></div>');
+		for(var i = 1; i < defaults.columns; i++) {
+			left += defaults.gutter;
+			$('body').append('<div class="grid_builder grid_vertical" style="left: ' + left  + 'px;"></div>');
+			left += colWidth; 
+			$('body').append('<div class="grid_builder grid_vertical" style="left: ' + left  + 'px;"></div>');
+		}
+	}
 }(jQuery));
--- popup.js
+++ popup.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 		}, function() {
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				chrome.tabs.sendMessage(tabs[0].id, {identifier: 'grid_builder_refresh'});
+
+				window.close();
 			});
 		});
     }, false);
